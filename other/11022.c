a4216cc43cd185038db2e2083a7efca513929df4
bit716 <sch030518@gmail.com>
2020-07-23 23:09:56 +0900

    A+B - 8

---
+++ other/11022.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i, T, A, B;
+    scanf("%d", &T);
+    for(i=1; i<=T; ++i) {
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d + %d = %d\n", i, A, B, A+B);
+    }
+    return 0;
+}

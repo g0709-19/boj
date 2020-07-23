09a721351ab7501d03eb53c25b9cb35647f5e2fa
bit716 <sch030518@gmail.com>
2020-07-23 23:09:38 +0900

    A+B - 5

---
+++ other/10952.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    while(1) {
+        scanf("%d %d", &A, &B);
+        if(A == 0 && B == 0)
+            break;
+        printf("%d\n", A+B);
+    }
+    return 0;
+}

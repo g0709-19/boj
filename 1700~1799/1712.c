d52daff686700cfc470dcdf8f09f0587472ab4f2
Jun <sch030518@gmail.com>
2020-09-14 01:17:18 +0900

    손익분기점

--- 1700~1799/1712.c
+++ 1700~1799/1712.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int x, A, B, C;
 	scanf("%d %d %d", &A, &B, &C);
-	if (B < C) {
+	if (B < C)
+	{
 		x = A / (C - B) + 1;
 	}
-	else {
+	else
+	{
 		x = -1;
 	}
 	printf("%d\n", x);

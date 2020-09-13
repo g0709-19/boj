e91c4f297bc8ffa72a12557d5f6f32816cab898f
Jun <sch030518@gmail.com>
2020-09-14 01:16:35 +0900

    벌집

--- 2200~2299/2292.c
+++ 2200~2299/2292.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int i, N, end = 1;
 	scanf("%d", &N);
-	if (N > 1) {
+	if (N > 1)
+	{
 		for (i = 1; 1; ++i) {
 			end += 6 * i;
 			if (N <= end) {
@@ -12,7 +14,8 @@ int main() {
 			}
 		}
 	}
-	else {
+	else
+	{
 		printf("%d", end);
 	}
 	return 0;

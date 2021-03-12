e7705b73f1def5d5bce94e48a9c74b4959298399
Jun <sch030518@gmail.com>
2021-03-13 05:52:53 +0900

    별 찍기 - 3

---
+++ 2400~2499/2440.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		for (int j = N - i; j > 0; --j)
+			cout << '*';
+		cout << '\n';
+	}
+	return 0;
+}

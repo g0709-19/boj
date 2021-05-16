c18b2d4eb3146f508c928fdc5d25202e3481cb3f
Jun <sch030518@gmail.com>
2021-05-16 19:09:24 +0900

    카드2

---
+++ 2100~2199/2164.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	queue<int> q;
+	for (int i = 1; i <= N; ++i) {
+		q.push(i);
+	}
+	while (q.size() > 1) {
+		q.pop();
+		if (q.size() <= 1)
+			break;
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front() << '\n';
+	return 0;
+}
